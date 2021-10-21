26d269eedc2eb8e209a614e1f6df26093b4863e5
Logan Oakley <Logan.t.oakley@gmail.com>
2021-10-20 22:53:26 -0500

    created some proof of concept processing

--- compvision.py
+++ compvision.py
@@ -7,18 +7,41 @@ import os
 
 
 # This is all sample OpenCV code
-path = r'Computer Vision.png'
+path = r'pacman_atarigame.png'
 
 # Reading an image in default mode
 image = cv.imread(path)
 
+
 # Window name in which image is displayed
 window_name = 'image'
+hsv_win_name = 'hsv image'
 
+hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+## mask of green (36,0,0) ~ (70, 255,255)
+mask1 = cv.inRange(hsv, (36, 0, 0), (70, 255,255))
+
+## mask o yellow (15,0,0) ~ (36, 255, 255)
+mask2 = cv.inRange(hsv, (15,0,0), (36, 255, 255))
+
+
+pacman = cv.inRange(hsv, (17,0,0), (17, 255, 255))
+ghostsAndDots = cv.inRange(hsv, (43, 0, 0), (43, 255,255))
+score =  cv.inRange(hsv, (64, 0, 0), (64, 255,255))
+## final mask and masked
+mask = cv.bitwise_or(mask1, mask2)
+target = cv.bitwise_and(hsv,hsv, mask=mask)
+target_pacman = cv.bitwise_and(hsv,hsv, mask=pacman)
+target_ghost = cv.bitwise_and(hsv,hsv, mask=ghostsAndDots)
+target_score = cv.bitwise_and(hsv,hsv, mask=score)
 # Using cv2.imshow() method
 # Displaying the image
-cv.imshow(window_name, image)
-
+cv.imshow(window_name,image)
+cv.imshow('Masked Image HSV', target)
+cv.imshow('Pac-Man Location', target_pacman)
+cv.imshow('Ghost location', target_ghost)
+cv.imshow('Score', target_score)
 # waits for user to press any key
 # (this is necessary to avoid Python kernel form crashing)
 cv.waitKey(0)
