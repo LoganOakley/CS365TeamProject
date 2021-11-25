b491c14d2344fcde41861557f5629fd0f5ef98ee
Logan Oakley <Logan.t.oakley@gmail.com>
2021-11-25 01:01:17 -0600

    corrected template matching

--- matchtemplatetest/basictest.py
+++ matchtemplatetest/basictest.py
@@ -1,11 +1,12 @@
 import cv2, pyautogui, numpy as np 
 
-game = cv2.imread('screenshot2.png', cv2.IMREAD_UNCHANGED)
-en1 = cv2.imread('enemy1.png', cv2.IMREAD_UNCHANGED)
-en2 = cv2.imread('enemy2.png', cv2.IMREAD_UNCHANGED)
-spider = cv2.imread('spider.png', cv2.IMREAD_UNCHANGED)
+game = cv2.imread('resources/MatchTemplateImages/screenshot 2.PNG', cv2.COLOR_BGR2GRAY)
+imgCopy= game.copy()
+en1 = cv2.imread('resources/MatchTemplateImages/enemy1.png', cv2.COLOR_BGR2GRAY)
+en2 = cv2.imread('resources/MatchTemplateImages/enemy2.png', cv2.COLOR_BGR2GRAY)
+spider = cv2.imread('resources/MatchTemplateImages/spider.png', cv2.COLOR_BGR2GRAY)
 
-result = cv2.matchTemplate(game, spider, cv2.TM_CCOEFF_NORMED)
+result = cv2.matchTemplate(imgCopy, spider, cv2.TM_CCOEFF_NORMED)
 min_val, max_val, min_index, max_index = cv2.minMaxLoc(result)
 
 
@@ -14,8 +15,8 @@ spider_h = spider.shape[0]
 
 top_left = max_index
 bottom_right = (top_left[0] + spider_w, top_left[1] + spider_h)
-cv2.rectangle(game, top_left, bottom_right, color=(255,0,0), thickness=2, lineType=cv2.LINE_4)
+cv2.rectangle(imgCopy, top_left, bottom_right, color=(255,0,0), thickness=2, lineType=cv2.LINE_4)
 
 
-cv2.imshow('Result', game)
+cv2.imshow('Result', imgCopy)
 cv2.waitKey()
